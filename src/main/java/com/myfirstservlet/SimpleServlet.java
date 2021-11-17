239f4147c4d7d600e3e166f75551b86acf61eddb
ToshitaC <toshitachaudhari17@gmail.com>
2021-11-17 16:57:55 +0530

    Added:First Servlet Program

---
+++ src/main/java/com/myfirstservlet/SimpleServlet.java
@@ -0,0 +1,23 @@
+package com.myfirstservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MyFirstServlet")
+
+public class SimpleServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3> Hello Everyone!! My First Servlet Program.</h3>");
+        out.close();
+    }
+}
---
+++ target/Demo/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello Everyone!! My First Servlet Program</h2>
+</body>
+</html>
